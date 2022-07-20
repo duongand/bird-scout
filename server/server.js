3c921ca2f30e21ab1f2fc92f341d2c016a9c3e81
duongand <duonganderson@gmail.com>
2022-07-20 13:04:50 -0400

    refactor Twitter API requests

--- server/server.js
+++ server/server.js
@@ -1,127 +1,87 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3001;
-const axios = require('axios');
-require('dotenv').config();
 
+const axios = require('axios');
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.BEARER}`;
+
+function getRecentTweets(query) {
+	return axios.get('https://api.twitter.com/2/tweets/search/recent', {
+		params: {
+			'query': query,
+			'expansions': 'author_id',
+			'tweet.fields': 'author_id,public_metrics,text',
+			'user.fields': 'id,name,username,profile_image_url'
+		}
+	}).catch((error) => {
+		console.log(error, 'Error retreiving recent tweets.');
+	});
+};
+
+function getFavoriteUsersInfo(usernames) {
+	return axios.get('https://api.twitter.com/2/users/by', {
+		params: {
+			'usernames': usernames,
+			'user.fields': 'profile_image_url'
+		}
+	}).catch((error) => {
+		console.log(error, 'Error pulling the favorite user information');
+	});
+};
+
+function getUserRecentTweet(authorId) {
+	return axios.get(`https://api.twitter.com/2/users/${authorId}/tweets`, {
+		params: {
+			'tweet.fields': 'created_at,public_metrics,author_id',
+			'user.fields': 'name,username,profile_image_url',
+			'expansions': 'author_id',
+			'media.fields': 'preview_image_url',
+		}
+		}).catch((error) => {
+			console.log(error, 'Error pulling the user recent timeline');
+		});
+};
+
+function mergeTweetData(tweetData, authorData) {
+	const authorProfileInfo = {};
+	for (const author of authorData) {
+		authorProfileInfo[author.id] = {
+			"name": author.name,
+			"username": author.username,
+			"profile_image_url": author.profile_image_url
+		};
+	};
+
+	const mergedTweetData = [];
+	for (let tweet of tweetData) {
+		mergedTweetData.push({
+			...tweet,
+			...authorProfileInfo[tweet.author_id]
+		});
+	};
+
+	return mergedTweetData;
+};
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.get('/api/recenttweets', async (req, res) => {
-	const submittedQuery = req.query.query;
-	const recentTweetsResponse = await axios.get('https://api.twitter.com/2/tweets/search/recent', {
-		params: {
-			'query': submittedQuery
-		},
-		headers: {
-			'Authorization': `Bearer ${process.env.BEARER}`
-		}
-	}).catch((error) => {
-		console.log(error);
-		res.sendStatus(500);
-	});
-
-	const tweetIdsString = recentTweetsResponse.data.data.map((recentTweet) => {
-		return recentTweet.id
-	}).join(',');
-
-	const tweetInformationResponse = await axios.get('https://api.twitter.com/2/tweets', {
-		params: {
-			'ids': tweetIdsString,
-			'tweet.fields': 'author_id,created_at,attachments,public_metrics',
-		},
-		headers: {
-			'Authorization': `Bearer ${process.env.BEARER}`
-		}
-	}).catch((error) => {
-		console.log(error);
-		res.sendStatus(500);
-	});
-
-	const tweetInformationData = tweetInformationResponse.data.data;
-	const tweetAuthorIdsString = tweetInformationData.map((tweet) => {
-		return tweet.author_id
-	}).join(',');
-
-	const authorInformationResponse = await axios.get('https://api.twitter.com/2/users', {
-		params: {
-			'ids': tweetAuthorIdsString,
-			'user.fields': 'profile_image_url'
-		},
-		headers: {
-			'Authorization': `Bearer ${process.env.BEARER}`
-		}
-	}).catch((error) => {
-		console.log(error);
-		res.sendStatus(500);
-	});
-
-	const authorInformation = authorInformationResponse.data.data;
-	const finalArray = [];
-	for (let i = 0; i < tweetInformationData.length; i++) {
-		finalArray.push({
-			...tweetInformationData[i],
-			'username': authorInformation[i].username,
-			'name': authorInformation[i].name,
-			'profile_image_url': authorInformation[i].profile_image_url,
-		});
-	};
-
-	res.send(finalArray);
+	const recentTweets = await getRecentTweets(req.query.query);
+	const mergedTweetData = mergeTweetData(recentTweets.data.data, recentTweets.data.includes.users);
+	res.send(mergedTweetData);
 });
 
 app.get('/api/getFavoriteUsers', async (req, res) => {
-	const favoriteUserInformation = await axios.get('https://api.twitter.com/2/users/by', {
-		params: {
-			'usernames': req.query.usernames,
-			'user.fields': 'profile_image_url'
-		},
-		headers: {
-			'Authorization': `Bearer ${process.env.BEARER}`
-		}
-	}).catch((error) => {
-		console.log(error);
-		res.sendStatus(500);
-	});
-	
-	res.send(favoriteUserInformation.data.data);
+	const favoriteUsers = await getFavoriteUsersInfo(req.query.usernames);
+	res.send(favoriteUsers.data.data)
 });
 
 app.get('/api/getFavoriteUserTimeline', async (req, res) => {
-	const favoriteUserTimeline = await axios.get(`https://api.twitter.com/2/users/${req.query.id}/tweets`, {
-	params: {
-		'tweet.fields': 'public_metrics'
-	},
-	headers: {
-		'Authorization': `Bearer ${process.env.BEARER}`
-	}
-	}).catch((error) => {
-		console.log(error);
-		res.sendStatus(500);
-	});
-
-	const authorInformation = await axios.get(`https://api.twitter.com/2/users/${req.query.id}`, {
-		params: {
-			'user.fields': 'profile_image_url'
-		},
-		headers: {
-			'Authorization': `Bearer ${process.env.BEARER}`
-		}
-	}).catch((error) => {
-		console.log(error);
-		res.sendStatus(500);
-	});
-
-	const profile = authorInformation.data.data;
-	const finalArray = favoriteUserTimeline.data.data.map((tweet) => ({
-		...tweet,
-		"profile_image_url": profile.profile_image_url,
-		"username": profile.username,
-		"name": profile.name
-	}));
-
-	res.send(finalArray);
+	const favoriteUserTimeline = await getUserRecentTweet(req.query.id);
+	const mergedTweetData = mergeTweetData(favoriteUserTimeline.data.data, favoriteUserTimeline.data.includes.users);
+	res.send(mergedTweetData);
 });
 
 app.listen(port, () => {
